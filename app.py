23ec1e97adf56ef52d7da1b5cab2493e4be1d143
Vasu Goel <alohanvasu@gmail.com>
2019-10-17 13:23:59 +0530

    Made some basic changes

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, flash, redirect, render_template, request, session, abort,send_from_directory
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS, cross_origin
+from flask_security import Security, login_required, SQLAlchemySessionUserDatastore
+from flask_login import LoginManager, login_user, current_user, logout_user
+from database import db_session, init_db
+from models import User, Role
+from forms import *
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = os.urandom(24)
+
+user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
+
+security = Security(app, user_datastore)
 
 app.config['DEFAULT_PARSERS'] = [
     'flask.ext.api.parsers.JSONParser',
@@ -13,12 +24,22 @@ app.config['DEFAULT_PARSERS'] = [
     'flask.ext.api.parsers.MultiPartParser'
 ]
 
-UPLOAD_FOLDER = 'C://Users//jain7//Documents//GitHub//RemoteVGAController//static'
+@app.before_first_request
+def create_db():
+    init_db()
+    db_session.commit()
+
+UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 cors = CORS(app,resources={r"/*":{"origins":"*"}})
 socketio = SocketIO(app)
 
+"""
+    Socket Methods Start
+"""
 @socketio.on('my event')
 def test_message(message):
     print(message)
@@ -32,6 +53,32 @@ def videoFromServer(message):
     #print("Here: " + message['data'])
     socketio.emit('displayVid', data=message['data'])
 
+"""
+    Socket Methods End
+"""
+
+
+"""
+    Auth methods Start
+"""
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+def create_user(email, username, password, name):
+    user_datastore.create_user(email=email, password=generate_password_hash(password, method='sha256'), username=username, name=name)
+    db_session.commit()
+
+
+"""
+    Auth Methods End
+"""
+
+
+"""
+    App Routes Start
+"""
 @app.route('/')
 def hello():
     socketio.emit('bringBackTheSlaves')
@@ -41,19 +88,41 @@ def hello():
         return render_template('start.html')
 
 @app.route('/hello')
+@login_required
 def hello1():
     return render_template('hello.html', name="Vasu")
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['password']=='password' and request.form['username']=='admin':
-        session['logged_in']=True
-        print(request.form['Name'])
-        session['name'] = request.form['Name']
-        session['description'] = request.form['Description']
+    if (request.method == 'POST'):
+        if request.form['password']=='password' and request.form['emailID']=='admin':
+            session['logged_in']=True
+            print(request.form['Name'])
+            session['name'] = request.form['Name']
+            session['description'] = request.form['Description']
+        else:
+            flash('Wrong Password!')
+        return hello()
     else:
-        flash('Wrong Password!')
-    return hello()
+        return render_template('login.html')
+
+@app.route('/createAccount', methods=['GET'])
+def createUserAccount():
+    return render_template('createAccount.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def do_admin_register():
+    signup_form = SignupForm(request.form)
+    if (request.method == 'POST'):
+        if signup_form.validate():
+            name = request.form.get('name')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            existing_user = User.query.filter_by(email=email).first()
+            if existing_user is None:
+                create_user(email=request.form['email'], username=request.form['username'], password=request.form['password'], name=request.form['name'])
+                return hello()
+    return createUserAccount()
 
 @app.route('/clientelle')
 def clientelle():
@@ -108,9 +177,10 @@ def client():
     temp=file.read()
     return render_template("second.html",query=temp)
 
+"""
+    App Routes End
+"""
 
 if __name__ == "__main__":
-    app.secret_key=os.urandom(12)
+    app.secret_key=os.urandom(16)
     socketio.run(app,host='0.0.0.0', debug=True)
-
-
